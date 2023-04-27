21190365277e64765989f828ff4f520a4f455634
Roanna Victorio <roannav@users.noreply.github.com>
2023-04-26 23:28:25 -0700

    transformOrigin tests

--- index.js
+++ index.js
@@ -7,3 +7,36 @@ gsap.from(".stick", {
   x: 200,
   duration: 5,
 });
+
+// The default for transform-origin is 50% 50%.
+// See what happens when we change the origin
+// before rotation.
+
+gsap.to(".origin_50_50", {
+  transformOrigin: "50% 50%",
+  rotation: 30,
+});
+
+gsap.to(".origin_0_0", {
+  transformOrigin: "0% 0%",
+  rotation: 30,
+});
+
+gsap.to(".origin_0_100", {
+  transformOrigin: "0% 100%",
+  rotation: 30,
+});
+
+gsap.to(".origin_100_100", {
+  transformOrigin: "100% 100%",
+  rotation: 30,
+});
+
+gsap.to(".origin_100_0", {
+  transformOrigin: "100% 0%",
+  rotation: 30,
+});
+
+gsap.to(".origin_default", {
+  rotation: 30,
+});
